cdff3ac9ac9c442c98bb14c55b26f83fd689cfda
Raafi <rafmhmd06@gmail.com>
2022-12-28 13:11:12 +0700

    + Enhancement print & cleanup

--- components/participantPrint.js
+++ components/participantPrint.js
@@ -1,65 +1,71 @@
-import React, { useRef, useState } from 'react';
-import { useReactToPrint } from 'react-to-print';
+import React, { useRef } from 'react';
+import ReactToPrint from 'react-to-print';
 import QRCode from 'react-qr-code';
 
 function ParticipantPrint(props) {
   const { data } = props || {};
-  const [participantClassName, setParticipantClassName] = useState('d-none');
-
-  console.log(data);
-
   const participantCardRef = useRef();
-  const print = useReactToPrint({
-    content: () => participantCardRef.current,
-  });
-
-  function handleOnPrint() {
-
-  }
 
   return (
     <>
-      <button className="btn btn-sm btn-success" onClick={() => print()}>Kartu Peserta</button>
+      <ReactToPrint
+        trigger={() => {
+          return (
+            <button className="btn btn-sm btn-success">Kartu Peserta</button>
+          );
+        }}
+        content={() => participantCardRef.current}
+        pageStyle={`@page { size: 90mm 125mm; }`}
+      />
       <div className="d-none">
         <div className="position-relative" ref={participantCardRef} style={{
           width: '340px',
+          margin: 0
         }}>
           <img src="/imgs/participant-card.jpg" alt="Paticipant Card" className="w-100" />
           <div
             className="position-absolute"
             style={{
-              bottom: '30px',
+              width: '125px',
+              top: '220px',
               left: '110px'
             }}
           >
             <div id="barcode" className="p-2 rounded bg-white border">
               <QRCode
-                size={115}
+                size={125}
                 style={{ height: 'auto', maxWidth: '100%', width: '100%' }}
                 value={data.barcode}
-                viewBox={`0 0 115 115`}
+                viewBox={`0 0 125 125`}
               />
             </div>
-            <div id="participant">
-              <p
-                className="text-center text-bold mb-1"
-                style={{ fontWeight: '600' }}
-              >
-                {data.barcode}
-              </p>
-              <p
-                className="mb-1 text-center"
-                style={{ fontWeight: 'bold', fontSize: '16px', textTransform: 'uppercase' }}
-              >
-                {data.participant.name}
-              </p>
-              <p
-                className="text-center"
-                style={{ fontWeight: '500', fontSize: '12px', textTransform: 'uppercase' }}
-              >
-                PC {data.participant.pc}
-              </p>
-            </div>
+          </div>
+          <div
+            id="participant"
+            className="position-absolute"
+            style={{
+              top: '355px',
+              width: '100%',
+            }}
+          >
+            <p
+              className="text-center text-bold mb-1"
+              style={{ fontWeight: '600', fontSize: '12px' }}
+            >
+              {data.barcode}
+            </p>
+            <p
+              className="mb-1 text-center"
+              style={{ fontWeight: 'bold', fontSize: '14px', textTransform: 'uppercase' }}
+            >
+              {data.participant.name}
+            </p>
+            <p
+              className="text-center"
+              style={{ fontWeight: '500', fontSize: '12px', textTransform: 'uppercase' }}
+            >
+              PC {data.participant.pc}
+            </p>
           </div>
         </div>
       </div>
