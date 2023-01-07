eb633c7df90b66d885516e6ed21dc78f134238f8
Raafi <rafmhmd06@gmail.com>
2023-01-07 23:50:19 +0700

    + try fix cam

--- pages/presences/[pid].js
+++ pages/presences/[pid].js
@@ -171,7 +171,7 @@ const Presences = () => {
                                                     delay={500}
                                                     onError={console.log}
                                                     style={{ width: '100%' }}
-                                                    facingMode="environment"
+                                                    facingMode="user"
                                                     onScan={(result) => {
                                                         if (result?.text && (result?.text != barcode)) {
                                                             setBarcode(result.text); // handle looping request
