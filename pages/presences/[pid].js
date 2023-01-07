7ab655e5d09c8aeaf876d10a43ed5f9eef2902d4
Hendar <hendarsundana@gmail.com>
2023-01-07 11:17:34 +0700

    +: enhance form submit presence

--- pages/presences/[pid].js
+++ pages/presences/[pid].js
@@ -21,18 +21,19 @@ const Presences = () => {
     const [schedule, setSchedule] = useState({});
     const [schedulePresence, setSchedulePresence] = useState({});
 
-    const scheduleData = async () => {
+    //define state
+    const [barcode, setBarcode] = useState("");
 
-        if (!pid && typeof pid === 'undefined') {
-            //redirect page dashboard
-            Router.push('/event');
-        } else {
-            //fetch user from Rest API
-            await axios.get(`${process.env.NEXT_PUBLIC_API_BACKEND}/v1/schedules/${pid}`)
-                .then((response) => {
-                    setSchedule(current => response.data.data);
-                })
-        }
+    //define state validation
+    const [validation, setValidation] = useState([]);
+
+    const scheduleData = async () => {
+        //fetch user from Rest API
+        await axios.get(`${process.env.NEXT_PUBLIC_API_BACKEND}/v1/schedules/${pid}`)
+            .then((response) => {
+                setSchedule(current => response.data.data);
+            })
+
     }
 
     const presenceData = async (id) => {
@@ -44,14 +45,68 @@ const Presences = () => {
             })
     }
 
-    //hook useEffect
-    useEffect(() => {
-        //check token
-        if (Cookies.get('token')) {
-            scheduleData();
-            presenceData(pid);
+    const resetFrom = async () => {
+        setBarcode("");
+    }
+
+    const resetValidation = async () => {
+        var delayInMilliseconds = 2000; //2 second
+        setTimeout(function () {
+            //your code to be executed after 2 second
+            setValidation({
+                message: null,
+                success: null
+            });
+        }, delayInMilliseconds);
+    }
+
+    const submitHandler = async (e) => {
+        e.preventDefault();
+
+        //initialize formData
+        const data = {
+            barcode: barcode,
+            schedule_id: schedule._id
         }
 
+
+        //set axios header dengan type Authorization + Bearer token
+        axios.defaults.headers.common['Authorization'] = `Bearer ${Cookies.get('token')}`
+
+        //send data to server
+        await axios.post(`${process.env.NEXT_PUBLIC_API_BACKEND}/v1/schedule-presences`, data)
+            .then((response) => {
+                // alert();
+                setValidation({
+                    success: `Peserta dengan Barcode ${barcode} berhasil ditambahkan!`
+                });
+                // router.reload(window.location.pathname)
+                resetFrom()
+                presenceData(pid);
+                resetValidation();
+            }).catch((error) => {
+                resetFrom()
+                //assign error to state "validation"
+                error.response.data.message = error.response.data.error;
+                setValidation({
+                    message : `Peserta dengan Barcode ${barcode} sudah menghadiri sesi ini!`
+                });
+                resetValidation();
+            })
+    };
+
+    //hook useEffect
+    useEffect(() => {
+        if (!pid && typeof pid === 'undefined') {
+            //redirect page dashboard
+            Router.push('/event');
+        } else {
+            //check token
+            if (Cookies.get('token')) {
+                scheduleData();
+                presenceData(pid);
+            }
+        }
     }, []);
 
     return (
@@ -73,7 +128,35 @@ const Presences = () => {
                             <h5 className='font-weight-light'>{schedule.name}</h5>
                         </div>
 
-                        <SubmitPresence schedule={schedule} />
+                        {/* <SubmitPresence schedule={schedule} /> */}
+
+                        <div className="card border-0 rounded shadow-lg">
+                            <div className="card-body">
+                                {
+                                    validation.message && (
+                                        <div className="alert alert-danger">
+                                            {validation.message}
+                                        </div>
+                                    )
+                                }
+                                {
+                                    validation.success && (
+                                        <div className="alert alert-success">
+                                            {validation.success}
+                                        </div>
+                                    )
+                                }
+                                <form onSubmit={submitHandler}>
+                                    <div className="mb-3">
+                                        <input type="text" className="form-control" value={barcode} onChange={(e) => setBarcode(e.target.value)} placeholder="Masukkan Barcode" required />
+                                    </div>
+                                    <div className="d-flex flex-row btn-group ">
+                                        <button type="button" className="btn btn-sm btn-info"><i className="bi bi-qr-code-scan"></i> Scan Barcode</button>
+                                        <button type="submit" className="btn btn-sm btn-primary"><i className="bi bi-calendar-check"></i> Hadir</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
 
                         {schedulePresence.length > 0 ?
                             <PresencesList participants={schedulePresence} />
