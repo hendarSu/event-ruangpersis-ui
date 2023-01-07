0a9703796c113dfc0fde84e3c7afb636574e7b51
Hendar <hendarsundana@gmail.com>
2023-01-07 21:29:27 +0700

    +: update leyouting voting 2

--- components/participant.js
+++ components/participant.js
@@ -29,7 +29,6 @@ function Participant({ participants }) {
                             <td>{data.barcode}</td>
                             <td>{data.participant.npa}</td>
                             <td>{data.participant.name}</td>
-                            {/* <td>{data.participant.pd}</td> */}
                             <td>{data.participant.pc}</td>
                             <td className="text-center">
                                 <ParticipantPrint data={data} />
